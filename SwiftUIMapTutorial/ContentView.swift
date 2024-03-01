46e9813f7c29df40a0c4f7d969adc02c2745ed52
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-03-01 10:54:28 -0500

    - create extensions for userLocation and userRegion - create property for cameraPosition

--- SwiftUIMapTutorial/ContentView.swift
+++ SwiftUIMapTutorial/ContentView.swift
@@ -6,8 +6,10 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct ContentView: View {
+    @State private var cameraPosition: MapCameraPosition = .region(.userRegion)
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +18,20 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+    }
+}
+
+extension CLLocationCoordinate2D {
+    static var userLocation: CLLocationCoordinate2D {
+        return .init(latitude: 25.781441, longitude: -80.188332)
+    }
+}
+
+extension MKCoordinateRegion {
+    static var userRegion: MKCoordinateRegion {
+        return .init(center: .userLocation,
+                    latitudinalMeters: 10000,
+                    longitudinalMeters: 10000)
     }
 }
 
