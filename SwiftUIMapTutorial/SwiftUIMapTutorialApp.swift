87af5fcc2d690a63288c3162ce086a14b4756e40
Danielle Lewis <103973410+dlewisdev@users.noreply.github.com>
2024-03-01 10:47:16 -0500

    Initial Commit

---
+++ SwiftUIMapTutorial/SwiftUIMapTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapTutorialApp.swift
+//  SwiftUIMapTutorial
+//
+//  Created by Danielle Lewis on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
